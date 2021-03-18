ce3b51a64fbda696523b877b59b34c3953ba64fa
linzs02 <linzs02@mingyuanyun.com>
2021-03-18 13:10:04 +0800

    聊天室-bug fix

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let commonConfig = require('../config/common-prod');
 
 router.get('/', function (req, res, next) {
-    let roomId = commonConfig.DEFAULT_ROOM_Id;
+    let roomId = commonConfig.DEFAULT_ROOM_ID;
     res.render('chatroom', {'title': '房间号: ' + roomId, 'roomId': roomId});
 });
 
